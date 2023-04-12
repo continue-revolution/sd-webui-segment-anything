e6a77e6690b80abd79ff712143005925fb863a1f
Chengsong Zhang <continuerevolution@gmail.com>
2023-04-12 11:23:00 +0800

    enable auto download

--- install.py
+++ install.py
@@ -1,8 +1,8 @@
 import launch
 import os
 
-req_file = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), "requirements.txt")
+current_dir = os.path.dirname(os.path.realpath(__file__))
+req_file = os.path.join(current_dir, "requirements.txt")
 
 with open(req_file) as file:
     for lib in file:
@@ -10,3 +10,7 @@ with open(req_file) as file:
         if not launch.is_installed(lib):
             launch.run_pip(
                 f"install {lib}", f"sd-webui-segment-anything requirement: {lib}")
+
+launch.git_pull_recursive(current_dir)
+dino_dir = os.path.join(current_dir, "GroundingDINO")
+launch.run_pip(f"install -e {dino_dir}", "sd-webui-segment-anything requirement: GroundingDINO")
